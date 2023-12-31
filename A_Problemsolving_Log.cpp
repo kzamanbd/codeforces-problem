1a8855033733913d7ac3c2ab15acf7f42dd05ff5
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-12-31 10:53:14 +0600

    update gitignore

---
+++ A_Problemsolving_Log.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int testCase;
+    cin >> testCase;
+    while (testCase--)
+    {
+        int n;
+        cin >> n;
+        }
+    return 0;
+}
